4e0b25e7258630c2048eb226549055ecabecd93e
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-10-10 10:48:19 -0300

    fix: arruma modal

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,35 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import Context from '../context/Context';
+import { getPeople } from '../service/getAPI';
 
 Modal.setAppElement('#root');
 
 function Details() {
   const {
-    modalIsOpen, setIsOpen, filmId,
+    modalIsOpen, setModalIsOpen, selectedFilm,
   } = useContext(Context);
 
-  // const [people, setPeople] = useState([]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    const getAllPeople = async () => {
+      const arrayPeople = selectedFilm.people.map(async (pessoa) => {
+        if (pessoa !== 'https://ghibliapi.herokuapp.com/people/') {
+          const result = await getPeople(pessoa);
+          return result;
+        }
+        return arrayPeople;
+      });
+      const resolvePeople = await Promise.all(arrayPeople);
+      setPeople(resolvePeople);
+    };
+    getAllPeople();
+  }, []);
 
   const closeModal = () => {
-    setIsOpen(false);
+    setModalIsOpen(false);
   };
-
-  // const getPeoples = async (pessoa) => {
-  //   if (filmId) {
-  //     const results = await fetch(`${pessoa}`).then((response) => response.json());
-
-  //     setPeople(results);
-  //     console.log('teste film', results);
-  //   }
-  // };
-
-  // const getPeople = () => {
-  //   if (filmId) {
-  //     filmId.people.forEach((pessoa) => getPeoples(pessoa));
-  //   }
-  // };
-  // getPeople();
 
   return (
     <Modal
@@ -38,15 +38,23 @@ function Details() {
       onRequestClose={closeModal}
     >
       <section>
-        {filmId
+        {selectedFilm
           ? (
             <section>
-              <img src={filmId.movie_banner} alt="Banner do filme." />
-              <h1>{filmId.title}</h1>
-              <p>{filmId.description}</p>
+              <img src={selectedFilm.movie_banner} alt="Banner do filme." />
+              <h1>{selectedFilm.title}</h1>
+              <p>{selectedFilm.description}</p>
               <p>
-                { `Produtor: ${filmId.producer}` }
+                {`Produtor: ${selectedFilm.producer}`}
               </p>
+              {
+                people && people.map((e) => (
+                  <p key={e.name}>
+                    {`nome: ${e.name}`}
+                    {` age: ${e.age}`}
+                  </p>
+                ))
+              }
             </section>
           )
           : false}
