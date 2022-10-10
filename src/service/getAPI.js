4d3350842ac4f101c807ffdc705469a474035bbf
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-10-10 10:40:33 -0300

    feat: adiciona service

---
+++ src/service/getAPI.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import { Builder } from 'builder-pattern';
+
+const ghibliAPI = axios.create({});
+
+const builderFilms = (filmes) => {
+  const result = filmes.map((film) => Builder()
+    .id(film.id)
+    .title(film.title)
+    .image(film.image)
+    .movie_banner(film.movie_banner)
+    .description(film.description)
+    .producer(film.producer)
+    .director(film.director)
+    .release_date(film.release_date)
+    .running_time(film.running_time)
+    .rt_score(film.rt_score)
+    .people(film.people)
+    .locations(film.locations)
+    .build());
+  return result;
+};
+
+const getAllFilms = async () => {
+  const response = ghibliAPI.get('https://ghibliapi.herokuapp.com/films')
+    .then(async (res) => {
+      if (res && res.data) {
+        builderFilms(res.data);
+      }
+    })
+    .catch(() => false);
+  return response;
+};
+
+const builderPeople = (peoples) => Builder()
+  .name(peoples.name)
+  .age(peoples.age)
+  .build();
+
+const getPeople = async (url) => {
+  const response = ghibliAPI.get(url)
+    .then((res) => {
+      const result = builderPeople(res.data);
+      return result;
+    })
+    .catch(() => false);
+  return response;
+};
+
+const getAllLocations = async (url) => {
+  console.log('teste locations', url);
+};
+
+export {
+  getAllFilms,
+  getPeople,
+  getAllLocations,
+};
