0bc2753585797390e5695490950ea0220b88584b
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-10-10 10:22:09 -0300

    fix/refactor: arruma provider

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,104 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
 function Provider({ children }) {
-  const [filterByName, setFilterByName] = useState();
-  const [people, setPeople] = useState([]);
-  const [filterPeoples, setFilterPeoples] = useState([]);
-  const [location, setLocation] = useState([]);
-  const [filterLocations, setFilterLocations] = useState([]);
-  const [films, setFilms] = useState([]);
+  const [allFilms, setAllFilms] = useState([]);
   const [filterFilms, setfilterFilms] = useState([]);
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [filmId, setFilmId] = useState();
-
-  const filmById = async (id) => {
-    const results = await fetch(`https://ghibliapi.herokuapp.com/films/${id}`).then((response) => response.json());
-    return results;
-  };
-
-  useEffect(() => {
-    const getAllFilms = async () => {
-      const results = await fetch('https://ghibliapi.herokuapp.com/films').then((response) => response.json());
-      results.map((film) => filmById(film.id));
-      setFilms(results);
-      setfilterFilms(results);
-    };
-    getAllFilms();
-  }, []);
-
-  // useEffect(() => {
-  //   const filterApiName = films.filter(
-  //     (film) => film.title.toLowerCase()
-  //       .includes(filterByName.toString().toLowerCase()),
-  //   );
-  //   setfilterFilms(filterApiName === [] ? films : filterApiName);
-  // }, [filterByName]);
-
-  // useEffect(() => {
-  //   const getPeople = async () => {
-  //     const results = await fetch('https://ghibliapi.herokuapp.com/people').then((response) => response.json());
-  //     setPeople(results);
-  //     setFilterPeoples(results);
-  //   };
-  //   getPeople();
-  // }, []);
-
-  useEffect(() => {
-    const filterApiName = people.filter(
-      (peop) => peop.name.toLowerCase()
-        .includes(filterByName.toString().toLowerCase()),
-    );
-    setFilterPeoples(filterApiName === [] ? people : filterApiName);
-  }, [filterByName]);
-
-  useEffect(() => {
-    const getLocal = async () => {
-      const results = await fetch('https://ghibliapi.herokuapp.com/locations').then((response) => response.json());
-      setLocation(results);
-      setFilterLocations(results);
-    };
-    getLocal();
-  }, []);
-
-  useEffect(() => {
-    const filterApiName = location.filter(
-      (local) => local.name.toLowerCase()
-        .includes(filterByName.toString().toLowerCase()),
-    );
-    setFilterLocations(filterApiName === [] ? location : filterApiName);
-  }, [filterByName]);
+  const [selectedFilm, setSelectedFilm] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState();
 
   const contextValue = React.useMemo(() => ({
-    filterByName,
-    setFilterByName,
-    filterPeoples,
-    setFilterPeoples,
-    people,
-    setPeople,
-    location,
-    setLocation,
-    filterLocations,
-    setFilterLocations,
-    films,
-    setFilms,
+    allFilms,
+    setAllFilms,
     filterFilms,
     setfilterFilms,
+    selectedFilm,
+    setSelectedFilm,
     modalIsOpen,
-    setIsOpen,
-    filmId,
-    setFilmId,
+    setModalIsOpen,
   }), [
-    filterByName,
-    filterPeoples,
-    people,
-    location,
-    filterLocations,
-    films,
-    filterFilms,
     modalIsOpen,
-    filmId,
+    allFilms,
+    filterFilms,
+    selectedFilm,
   ]);
 
   return (
