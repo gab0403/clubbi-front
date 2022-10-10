07e331daacd16b4cf701d144003978336dcafd4c
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-10-10 10:25:09 -0300

    fix/refactor: arruma header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,11 @@
 import React from 'react';
-// import Context from '../context/Context';
 import logo from '../images/logo.png';
 import '../styles/Header.css';
 
 function Header() {
-  //  const { getFilms, getActors, getLocations } = useContext(Context);
-
   return (
     <header className="header">
       <img data-testid="img-header" className="header-img" src={logo} alt="Logo do Estudio Ghibli" />
-      {/* <section className="header-section">
-        <button
-          className="header-button"
-          data-testid="button-films"
-          type="button"
-          onClick={getFilms}
-        >
-          Filmes
-        </button>
-        <button
-          className="header-button"
-          data-testid="button-actors"
-          type="button"
-          onClick={getActors}
-        >
-          Atores
-        </button>
-        <button
-          className="header-button"
-          data-testid="button-locations"
-          type="button"
-          onClick={getLocations}
-        >
-          Locais
-        </button>
-      </section> */}
     </header>
   );
 }
