e68b088cfaae7768f08f273c2adf65a7ca7238a6
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-10-13 16:09:59 -0300

    test: corrige espaçamento

--- cypress/integration/01-header.spec.js
+++ cypress/integration/01-header.spec.js
@@ -1,11 +1,11 @@
 describe('01- Header', () => {
-    const hasHeader = () => {
-      cy.get('[data-testid="img-header"]');
-    };
-  
-    it('Verifica se há o elementos img.', () => {
-      cy.visit('http://localhost:3000');
-  
-      hasHeader();
+  const hasHeader = () => {
+    cy.get('[data-testid="img-header"]');
+  };
+
+  it('Verifica se há o elementos img.', () => {
+    cy.visit('http://localhost:3000');
+
+    hasHeader();
   });
 });
--- cypress/integration/02-films.spec.js
+++ cypress/integration/02-films.spec.js
@@ -1,29 +1,24 @@
 describe('02- Films', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000');
-    });
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
 
-    it('Verifica se o input de pesquisa tem o comportamento padrão', () => {
-  
-      cy.get('[data-testid="input-search"]').should('be.visible').type('Castle in the Sky');
-    });
+  it('Verifica se o input de pesquisa tem o comportamento padrão', () => {
+    cy.get('[data-testid="input-search"]').should('be.visible').type('Castle in the Sky');
+  });
 
+  it('Verifica se os cards de filmes estão renderizados', () => {
+    cy.get('[data-testid="result-card"]').should('be.visible');
+  });
 
-    it('Verifica se os cards de filmes estão renderizados', () => {
+  it('Verifica se o card possui os elementos: img, h1, e p', () => {
+    cy.get('[data-testid="img-card"]').should('be.visible');
+    cy.get('[data-testid="title-card"]').should('be.visible');
+    cy.get('[data-testid="element-card"]').should('be.visible');
+  });
 
-        cy.get('[data-testid="result-card"]').should('be.visible');
-    });
-
-    it('Verifica se o card possui os elementos: img, h1, e p', () => {
-  
-        cy.get('[data-testid="img-card"]').should('be.visible');
-        cy.get('[data-testid="title-card"]').should('be.visible');
-        cy.get('[data-testid="element-card"]').should('be.visible');
-    });
-
-    it('Verifica se o card possui um botão que abre a modal', () => {
-  
-        cy.get('[data-testid="button-details"]').should('be.visible');
-        cy.get('[data-testid="button-details"]', { timeout: 10000 }).eq(0).click({ multiple: true });
-    });
+  it('Verifica se o card possui um botão que abre a modal', () => {
+    cy.get('[data-testid="button-details"]').should('be.visible');
+    cy.get('[data-testid="button-details"]', { timeout: 10000 }).eq(0).click({ multiple: true });
+  });
 });
