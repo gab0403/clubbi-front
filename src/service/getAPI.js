073868254103f8a4c31e3e4f016ac5766a848897
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-10-13 10:17:26 -0300

    feat/perf: adiciona get de locations e melhora a performace de getAPI

--- src/service/getAPI.js
+++ src/service/getAPI.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { Builder } from 'builder-pattern';
+import { getSavedInLocalStorage } from '../helpers/localStorage';
 
 const ghibliAPI = axios.create({});
 
-const builderFilms = (filmes) => {
-  const result = filmes.map((film) => Builder()
+const builderFilms = (films) => {
+  const result = films.map((film) => Builder()
     .id(film.id)
     .title(film.title)
     .image(film.image)
@@ -21,7 +22,7 @@ const builderFilms = (filmes) => {
   if (result) {
     return result;
   }
-  return result;
+  return false;
 };
 
 const getAllFilms = async () => {
@@ -31,38 +32,102 @@ const getAllFilms = async () => {
   return response;
 };
 
-const builderPeople = (peoples) => Builder()
+const builderPeople = (filmId, peoples) => Builder()
+  .filmId(filmId)
   .name(peoples.name)
   .age(peoples.age)
   .build();
 
-const getPeople = async (url) => {
+const getPeople = async (filmId, url) => {
   const response = ghibliAPI.get(url)
     .then((res) => {
-      const result = builderPeople(res.data);
+      const result = builderPeople(filmId, res.data);
       return result;
     })
     .catch(() => false);
   return response;
 };
 
-// const builderLocations = (locations) => Builder()
-//   .name(locations.name)
-//   .build();
+const getFilmId = (films) => {
+  const ids = films && films.map((film) => film.substring(38, film.length));
+  return ids[0];
+};
 
-// const getLocations = async (url) => {
-//   const response = ghibliAPI.get(url)
-//     .then((res) => {
-//       const result = builderLocations(res.data);
-//       console.log(result);
-//       return result;
-//     })
-//     .catch(() => false);
-//   return response;
-// };
+const builderAllPeoples = (peoples) => {
+  const result = peoples.map((people) => Builder()
+    .id(people.id)
+    .name(people.name)
+    .filmId(getFilmId(people.films))
+    .age(people.age)
+    .build());
+  if (result) {
+    return result;
+  }
+  return false;
+};
+
+const getAllPeoples = async () => {
+  const response = ghibliAPI.get('https://ghibliapi.herokuapp.com/people')
+    .then(async (res) => builderAllPeoples(res.data))
+    .catch(() => false);
+  return response;
+};
+
+const builderAllLocations = (locations) => {
+  const result = locations.map((location) => Builder()
+    .id(location.id)
+    .name(location.name)
+    .filmId(getFilmId(location.films))
+    .build());
+  if (result) {
+    return result;
+  }
+  return false;
+};
+
+const getAllLocations = async () => {
+  const response = ghibliAPI.get('https://ghibliapi.herokuapp.com/locations')
+    .then(async (res) => builderAllLocations(res.data))
+    .catch(() => false);
+  return response;
+};
+
+const builderCompleteMovies = (films, peop, locations) => {
+  const result = films && films.map((film) => Builder()
+    .id(film.id)
+    .title(film.title)
+    .image(film.image)
+    .movie_banner(film.movie_banner)
+    .description(film.description)
+    .producer(film.producer)
+    .director(film.director)
+    .release_date(film.release_date)
+    .running_time(film.running_time)
+    .rt_score(film.rt_score)
+    .people(peop.filter((peo) => peo.filmId === film.id))
+    .locations(locations.filter((local) => local.filmId === film.id))
+    .build());
+  if (result) {
+    return result;
+  }
+  return false;
+};
+
+const getCompletedMovies = () => {
+  const resultFilms = JSON.parse(getSavedInLocalStorage('filmes'));
+  const resultPeoples = JSON.parse(getSavedInLocalStorage('pessoas'));
+  const resultLocations = JSON.parse(getSavedInLocalStorage('locais'));
+  if (resultFilms && resultPeoples && resultLocations) {
+    const filmesCompletos = builderCompleteMovies(resultFilms, resultPeoples, resultLocations);
+    return filmesCompletos;
+  }
+  return false;
+};
 
 export {
   getAllFilms,
   getPeople,
-  // getLocations,
+  getAllPeoples,
+  getAllLocations,
+  getCompletedMovies,
 };
